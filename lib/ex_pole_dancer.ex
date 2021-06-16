2a4bb4d0bf7693d9326065482221ccbc8d9875d6
Maarten Faddegon <github@maartenfaddegon.nl>
2021-06-16 19:26:15 +0200

    mix new

---
+++ lib/ex_pole_dancer.ex
@@ -0,0 +1,18 @@
+defmodule ExPoleDancer do
+  @moduledoc """
+  Documentation for `ExPoleDancer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExPoleDancer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_pole_dancer_test.exs
@@ -0,0 +1,8 @@
+defmodule ExPoleDancerTest do
+  use ExUnit.Case
+  doctest ExPoleDancer
+
+  test "greets the world" do
+    assert ExPoleDancer.hello() == :world
+  end
+end
