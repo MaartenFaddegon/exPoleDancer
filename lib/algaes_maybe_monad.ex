0dc8e0b65d232061acbb4971e22c4236be068d14
Maarten Faddegon <github@maartenfaddegon.nl>
2021-06-16 21:20:12 +0200

    algeas maybe monad

---
+++ lib/algaes_maybe_monad.ex
@@ -0,0 +1,38 @@
+defmodule AlgaesMaybeMonad do
+  alias Algae.Maybe
+  use Witchcraft
+
+  def land_left n, {left, right} do
+    if abs((left + n) - right) < 4 do
+      Maybe.new({left + n, right})
+    else
+      Maybe.new()
+    end
+  end
+
+  def land_right n, {left, right} do
+    if abs(left - (right + n)) < 4 do
+      Maybe.new({left, right + n})
+    else
+      Maybe.new()
+    end
+  end
+
+  def example1 do
+    Maybe.new({0,0})
+    >>> fn x -> land_right(2, x) end
+    >>> fn x -> land_left(2, x) end
+    >>> fn x -> land_right(2, x) end
+  end
+  # => %Maybe.Just{just: {2, 4}}
+
+  def example2 do
+    Maybe.new({0,0})
+    >>> fn x -> land_left(1, x) end
+    >>> fn x -> land_right(4, x) end
+    >>> fn x -> land_left(-1, x) end
+    >>> fn x -> land_right(-2, x) end
+  end
+  # => %Maybe.Nothing{}
+
+end
---
+++ test/algeas_maybe_monad_test.exs
@@ -0,0 +1,13 @@
+defmodule AlgeasMaybeMonadTest do
+  use ExUnit.Case
+  alias Algae.Maybe
+
+  test "maybe monad example 1" do
+    assert AlgaesMaybeMonad.example1 == %Maybe.Just{just: {2, 4}}
+  end
+
+  test "maybe monad example 2" do
+    assert AlgaesMaybeMonad.example2 == %Maybe.Nothing{}
+  end
+
+end
